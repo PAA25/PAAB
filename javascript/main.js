355e6b6279740486108608d65f90d5624c992bd1
PAA <paolaalessandraart@gmail.com>
2025-12-31 16:05:26 -0600

    Adding css

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+setTimeout(function() {mainDivWrapper(); }, 5000); 
+
+function mainDivWrapper(){
+  var mainDiv = document.getElementsByClassName("wrapper");
+  fadeIn(mainDiv);
+}
+
+function fadeIn(mainDiv) {
+    mainDiv[0].style.opacity = 0;
+    var showingImage = function () {
+        mainDiv[0].style.opacity = +mainDiv[0].style.opacity + 0.01;
+        if (+mainDiv[0].style.opacity < 1) {
+            (window.requestAnimationFrame && requestAnimationFrame(showingImage)) || setTimeout(showingImage, 16)
+        }
+    };
+    showingImage();
+}
